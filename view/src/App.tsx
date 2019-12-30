b39c898727933aa637797d875facf6623020d2cc
ah <ah224cy@student.lnu.se>
2019-12-30 16:30:09 +0100

    can switch between platforms now in view

--- view/src/App.tsx
+++ view/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './compenents/header/header';
+import StartSearch from './compenents/startSearch/startSearch'
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <Header/>
+      <StartSearch/>
     </div>
   );
 }
--- view/src/compenents/startSearch/startSearch.tsx
+++ view/src/compenents/startSearch/startSearch.tsx
@@ -1,11 +1,30 @@
 import React, { useState } from 'react';
 
-const StartSearch:(props:any) => JSX.Element = (props:any) => {
-  const[format, setFormat] = useState('pc');
+const StartSearch:(props:any) => JSX.Element = (props) => {
+  const[platform, setplatformor] = useState('steam');
+  const[platformNumber, setPlatformorNumber] = useState('4');
   
+  
+  const handleChange = (event:React.ChangeEvent<HTMLSelectElement>) => {
+    
+    const formatAndNumber:string[] = event.target.value.split(' ');
+    
+    console.log(formatAndNumber[0]);
+    console.log(formatAndNumber[1]);
+
+    setplatformor(formatAndNumber[0]);
+    setPlatformorNumber(formatAndNumber[1]);
+  }
   return(
   <div>
-
+    <h1>account name</h1>
+    <p>{platform} number:{platformNumber}</p>
+    <input id='playerSearch' type='text'></input>
+    <select onChange={(event:React.ChangeEvent<HTMLSelectElement>) => handleChange(event)} value={platform}>
+      <option value={['steam 4']}>steam</option>
+      <option value={['xbox 1']}>xbox</option>
+      <option value={['ps4 2']}>ps4</option>
+    </select>
   </div>
   );
 };
