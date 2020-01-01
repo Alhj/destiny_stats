bac52d07cfb63e4c84a8147b793bef8c49f2e96d
ah <ah224cy@student.lnu.se>
2020-01-01 14:30:46 +0100

    can get player profil

--- api/routes/GetCharacterData.js
+++ api/routes/GetCharacterData.js
@@ -2,25 +2,13 @@ require('dotenv').config()
 const side = require('express').Router()
 const fetch = require('node-fetch')
 
-side.route('/:platformNumber/:accountName')
+const getPlayerProfil = require('../lib/fetch/getPlayerProfil');
+
+
+side.route('/:membershipType/:displayName')
   .get(async (req, res) => {
 
-    let url = 'https://www.bungie.net/Platform/'
-
-    let config = {
-      headers:{
-        'Content-Type': 'application/json',
-        'X-API-Key': process.env.API_KEY,
-      }
-    }
-   
-    let respons = await fetch(url + `Destiny2/SearchDestinyPlayer/${req.params.platformNumber}/${req.params.accountName}`, config).then(res => res.json())
-
-    console.log(respons)
-
-   // respons = await fetch(url + `/Destiny2/${req.params.platformNumber}/Profile/${memberId}/ `, config).then(res => res.json())
-
-    //console.log(respons);
+    await getPlayerProfil(req.params.membershipType, req.params.displayName);
 
     res.status(401).send('hello world');
   })
