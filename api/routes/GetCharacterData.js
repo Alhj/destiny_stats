c5a995f8adb650422047534e76668ba7af8c983b
ah <ah224cy@student.lnu.se>
2020-01-01 13:39:09 +0100

    working on getting character info

--- api/routes/GetCharacterData.js
+++ api/routes/GetCharacterData.js
@@ -1,8 +1,27 @@
+require('dotenv').config()
 const side = require('express').Router()
+const fetch = require('node-fetch')
 
-side.route('/:platformNumber/:characterName')
+side.route('/:platformNumber/:accountName')
   .get(async (req, res) => {
-    console.log(req.params.characterName);
+
+    let url = 'https://www.bungie.net/Platform/'
+
+    let config = {
+      headers:{
+        'Content-Type': 'application/json',
+        'X-API-Key': process.env.API_KEY,
+      }
+    }
+   
+    let respons = await fetch(url + `Destiny2/SearchDestinyPlayer/${req.params.platformNumber}/${req.params.accountName}`, config).then(res => res.json())
+
+    console.log(respons)
+
+   // respons = await fetch(url + `/Destiny2/${req.params.platformNumber}/Profile/${memberId}/ `, config).then(res => res.json())
+
+    //console.log(respons);
+
     res.status(401).send('hello world');
   })
 
