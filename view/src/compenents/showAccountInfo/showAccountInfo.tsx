ef5b35ffbf081e90129a1f9145e9b64ba3639c9e
ah <ah224cy@student.lnu.se>
2020-01-03 12:42:53 +0100

    working on types

--- view/src/compenents/showAccountInfo/showAccountInfo.tsx
+++ view/src/compenents/showAccountInfo/showAccountInfo.tsx
@@ -6,13 +6,34 @@ import {getAccountInfo} from '../../helpers/getAccountInfo';
 const ShowAccountInfo:(props:any) => JSX.Element = (props) => {
 
 
-  const[isLoading, setIsLoading] = useState(true);
+  const[isLoading, setIsLoading] = useState(false);
 
-  return(
-    <div>
-      <h1>hello</h1>
-    </div>
-  );
+  
+  const loading = async () => {
+    const accountInfo = await new Promise((resolve, rejects) => {
+      setTimeout(() => {
+        resolve('hello');
+      }, 3000)
+    })
+
+    setIsLoading(true);
+  }
+
+  useEffect(() => {
+    loading();
+  });
+
+  if(isLoading) {
+    return(
+      <div>
+        <h1>test</h1>
+      </div>
+    )
+  } else {
+    return(
+      <Loading/>
+    )
+  }
 }
 
 export default ShowAccountInfo;
---
+++ view/src/types/types.ts
@@ -0,0 +1,5 @@
+export interface characters {
+  emblemBackgroundPath:string
+  classType:number
+  light:number
+}
