4f8719a69c69b09f23176e69d4f0873b4aec66ea
ah <ah224cy@student.lnu.se>
2020-01-03 18:27:48 +0100

    add types

--- view/src/types/types.ts
+++ view/src/types/types.ts
@@ -3,3 +3,50 @@ export interface characters {
   classType:number
   light:number
 }
+
+export interface charactersStats {
+  characterId: string
+  deleted: boolean
+  result: allPvP
+}
+
+interface allPvP {
+  activitiesEntered:activitiesEnteredStats
+  weaponKillsAutoRifle: weaponKillsStats
+  weaponKillsFusionRifle: weaponKillsStats
+  weaponKillsGrenade:weaponKillsStats
+  weaponKillsGrenadeLauncher: weaponKillsStats
+  weaponKillsHandCannon: weaponKillsStats
+  weaponKillsTraceRifle: weaponKillsStats
+  weaponKillsMachineGun:weaponKillsStats
+  weaponKillsMelee:weaponKillsStats
+  weaponKillsPulseRifle:weaponKillsStats
+  weaponKillsRocketLauncher:weaponKillsStats
+  weaponKillsScoutRifle:weaponKillsStats
+  weaponKillsShotgun:weaponKillsStats
+  weaponKillsSniper:weaponKillsStats
+  weaponKillsSubmachinegun:weaponKillsStats
+  weaponKillsSuper:weaponKillsStats
+  weaponKillsSideArm:weaponKillsStats
+  weaponKillsSword:weaponKillsStats
+  weaponKillsRelic:weaponKillsStats
+  weaponKillsBow:weaponKillsStats
+}
+
+ interface activitiesEnteredStats {
+    statId:string
+    basic: {
+      value:number,
+      displayValue:string
+    }
+ }
+
+ interface weaponKillsStats {
+  statsId:string
+  basic:weaponsStatsBasic
+}
+
+interface weaponsStatsBasic {
+  value: string
+  displayValue: string
+}
