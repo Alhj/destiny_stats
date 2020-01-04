998825ff40a8d43a1d47a71646897c399e7656c2
ah <ah224cy@student.lnu.se>
2020-01-04 10:26:27 +0100

    working on styling front page

--- view/src/compenents/startSearch/startSearch.tsx
+++ view/src/compenents/startSearch/startSearch.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import { Redirect } from 'react-router-dom';
+import SearchIcon from '@material-ui/icons/Search';
+import './startSearch.css'
 
 const StartSearch:(props:any) => JSX.Element = (props) => {
   const[platform, setplatformor] = useState('steam');
@@ -31,17 +33,17 @@ const StartSearch:(props:any) => JSX.Element = (props) => {
     <div>
       <h1>account name</h1>
       <p>{platform} number:{platformNumber}</p>
-      <form onSubmit={e => handelSubmit(e)}>
-        <input id='playerSearch' type='text' value={userName} onChange={(e:React.ChangeEvent<HTMLInputElement>) => handleInputChange(e)}/>
+      <div className='searchForm'>
+        <form onSubmit={e => handelSubmit(e)}>
         <select onChange={(event:React.ChangeEvent<HTMLSelectElement>) => handleChange(event)} value={platform}>
-          <option value={['steam 3']}>steam</option>
-          <option value={['xbox 1']}>xbox</option>
-          <option value={['ps4 2']}>ps4</option>
-        </select>
-
-        <br/>
-        <input type="submit" value='submit'/>
-      </form>
+            <option value={['steam 3']}>steam</option>
+            <option value={['xbox 1']}>xbox</option>
+            <option value={['ps4 2']}>ps4</option>
+          </select>
+          <input id='playerSearch' type='text' value={userName} placeholder='serach...' onChange={(e:React.ChangeEvent<HTMLInputElement>) => handleInputChange(e)}/>
+          <button type='submit'><SearchIcon fontSize='small'/></button>
+        </form>
+      </div>
     </div>
     );
   }
