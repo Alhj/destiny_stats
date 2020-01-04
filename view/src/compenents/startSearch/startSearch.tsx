bfa6db715295e25d60ffcf6f0a882449a0a62d8a
ah <ah224cy@student.lnu.se>
2020-01-04 18:51:21 +0100

    working on getting the select to change value

--- view/src/compenents/startSearch/startSearch.tsx
+++ view/src/compenents/startSearch/startSearch.tsx
@@ -10,6 +10,7 @@ const StartSearch:(props:any) => JSX.Element = (props) => {
   const[redirect, setRedirect] = useState(false);
   
   const handleChange:(event:React.ChangeEvent<HTMLSelectElement>) => void = (event:React.ChangeEvent<HTMLSelectElement>) => {
+    event.preventDefault();
     const formatAndNumber:string[] = event.target.value.split(' ');
 
     setplatformor(formatAndNumber[0]);
@@ -35,13 +36,12 @@ const StartSearch:(props:any) => JSX.Element = (props) => {
       <p>{platform} number:{platformNumber}</p>
       <div className='searchForm'>
         <form onSubmit={e => handelSubmit(e)}>
-        <select onChange={(event:React.ChangeEvent<HTMLSelectElement>) => handleChange(event)} value={platform}>
-            <option value={['steam 3']}>steam</option>
-            <option value={['xbox 1']}>xbox</option>
-            <option value={['ps4 2']}>ps4</option>
-          </select>
-          <input id='playerSearch' type='text' value={userName} placeholder='serach...' onChange={(e:React.ChangeEvent<HTMLInputElement>) => handleInputChange(e)}/>
-          <button type='submit'><SearchIcon fontSize='small'/></button>
+          <select onChange={event => handleChange(event)} value={platform}>
+              <option value={['steam 3']}>steam</option>
+              <option value={['xbox 1']}>xbox</option>
+              <option value={['ps4 2']}>ps4</option>
+            </select>
+            <input id='playerSearch' type='text' value={userName} placeholder='serach...' onChange={(e:React.ChangeEvent<HTMLInputElement>) => handleInputChange(e)}/>
         </form>
       </div>
     </div>
