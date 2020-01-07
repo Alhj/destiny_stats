a98d784d4a378ae7bb8eadbb6385c31ea70ec27c
ah <ah224cy@student.lnu.se>
2020-01-07 09:48:02 +0100

    add env settings

--- api/index.js
+++ api/index.js
@@ -5,6 +5,10 @@ require('dotenv').config()
 const app = express()
 
 const port = process.env.PORT || 3080
+const node_env = process.env.NODE_ENV || 'development'
+
+
+app.use('env', node_env)
 
 app.use('/character', require('./routes/CharacterData'))
 app.use('/apiKey', require('./routes/ApiKey'))
