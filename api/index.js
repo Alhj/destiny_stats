346f11717337ede145e14f703b538733d25d91d7
ah <ah224cy@student.lnu.se>
2020-01-16 13:20:29 +0100

    revert

--- api/index.js
+++ api/index.js
@@ -6,25 +6,13 @@ require('dotenv').config()
 const app = express()
 
 const port = process.env.PORT || 3080
-const node_env = process.env.NODE_ENV || 'development'
-
-const whiteList = ['http://destinystatsview.herokuapp.com/', 'http://localhost:3000/']
-
-const corsOptions = {
-  origin: (origin, callback) => {
-    if(whiteList.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allwed bt CORS'))
-    }
-  }
-}
+//const node_env = process.env.NODE_ENV || 'development'
 
 app.use(cors())
 
-app.use('/character', cors(corsOptions), require('./routes/CharacterData'))
-app.use('/apiKey', cors(corsOptions), require('./routes/ApiKey'))
-app.use('/stats', cors(corsOptions), require('./routes/AccountStats'))
-app.use('/profil', cors(corsOptions), require('./routes/accountProfil'))
+app.use('/character', require('./routes/CharacterData'))
+app.use('/apiKey', require('./routes/ApiKey'))
+app.use('/stats', require('./routes/AccountStats'))
+app.use('/profil', require('./routes/accountProfil'))
 
 app.listen(port, () => console.log(`listen on port: ${port}`))
