fef46b0e961ff1b4eeb0dfa8324064e7d0c86e2b
ah <ah224cy@student.lnu.se>
2020-01-16 10:18:15 +0100

    add cors url

--- api/index.js
+++ api/index.js
@@ -8,13 +8,23 @@ const app = express()
 const port = process.env.PORT || 3080
 const node_env = process.env.NODE_ENV || 'development'
 
+const whiteList = ['http://destinystatsview.herokuapp.com/', 'http://localhost:3000/']
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if(whiteList.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allwed bt CORS'))
+    }
+  }
+}
 
 app.use(cors())
-//app.use('env', node_env)
 
-app.use('/character', require('./routes/CharacterData'))
-app.use('/apiKey', require('./routes/ApiKey'))
-app.use('/stats', require('./routes/AccountStats'))
-app.use('/profil', require('./routes/accountProfil'))
+app.use('/character', cors(corsOptions), require('./routes/CharacterData'))
+app.use('/apiKey', cors(corsOptions), require('./routes/ApiKey'))
+app.use('/stats', cors(corsOptions), require('./routes/AccountStats'))
+app.use('/profil', cors(corsOptions), require('./routes/accountProfil'))
 
 app.listen(port, () => console.log(`listen on port: ${port}`))
